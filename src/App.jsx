fff549092bfa95afdf17ea2e6778bbd0e6fe19f7
testninja50 <testninja50@gmail.com>
2024-03-02 23:34:01 +0500

    daraz-changes-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
-  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="mainDiv">
-        <img
-          src={
-            "https://live.staticflickr.com/65535/51694698128_58386fdf21_b.jpg"
-          }
-          className="abrarLogo logo"
-          alt="Vite logo"
-        />
-        <h1>Abrar Hussain</h1>
+      <div className="main">
+        <nav>
+          <div className="topbar">
+            <div className="topbar-left">
+              <ul>
+                <li><a href="#">Become a Seller</a></li>
+                <li><a href="#">Daraz Affiliate Program</a></li>
+                <li><a href="#">Help & Support</a></li>
+                <li><a href="#">Help & Support</a></li>
+                <li><a href="#">Daraz Logistics Partner</a></li>
+              </ul>
+            </div>
+            <div className="topbar-right">
+              <button>
+                <img src="https://img.alicdn.com/imgextra/i1/O1CN01Ie2YnK1JmZ1mL3fY5_!!6000000001071-2-tps-60-60.png" alt="" />
+                <span>Save more on App</span>
+              </button>
+            </div>
+          </div>
+          <div className="navbar">
+            <img src="https://icms-image.slatic.net/images/ims-web/e650d6ca-1841-4646-b0e9-4ddbf2beb731.png" alt="Daraz logo" />
+            <input type="search" placeholder="Search in Daraz" />
+            <div className="buttons">
+              <button className="logn">Login</button>
+              <span> | </span>
+              <button className="logn">Sign Up</button>
+              <button className="logn">&#9762; EN</button>
+              <button className="logn">&#9750;</button>
+            </div>
+          </div>
+        </nav>
+        <div className="header">
+          <aside>
+            <ul>
+              <li><a href="#">Groceries & Pets</a></li>
+              <li><a href="#">Groceries & Pets</a></li>
+              <li><a href="#">Health & Beauty</a></li>
+              <li><a href="#">Men's Fashion</a></li>
+              <li><a href="#">Women's Fashion</a></li>
+              <li><a href="#">Mother & Baby</a></li>
+              <li><a href="#">Home & Lifestyle</a></li>
+              <li><a href="#">Electronic Devices</a></li>
+              <li><a href="#">Electronic Accessories</a></li>
+              <li><a href="#">TV & Home Appliances</a></li>
+              <li><a href="#">Sports & Outdoor</a></li>
+              <li><a href="#">Watches, Bags & Jewellery</a></li>
+              <li><a href="#">Automotive & Motorbike</a></li>
+            </ul>
+          </aside>
+          <div className="slider">
+            <img src="https://icms-image.slatic.net/images/ims-web/1ec33dcd-79b9-4719-a711-5b77775c1b16.jpeg" alt="" />
+          </div>
+        </div>
       </div>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div> */}
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 }
